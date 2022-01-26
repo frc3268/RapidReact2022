79663faa70e759c5094ecfd1a4d20736c7700488
Matthew Epshtein <epshteinmatthew@gmail.com>
2022-01-26 13:55:39 -0800

    Robotmap temp port updates

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -4,5 +4,9 @@ public class RobotMap {
     //OI-Buttons on joystick(?)
 
 
-    //Drive-Motor controllers
+    //Drive-Motor controllers(Replace with actual ports at some point)
+    public static final int SPARK_driveLeftFront = 1;
+	public static final int SPARK_driveLeftBack = 1;
+	public static final int SPARK_driveRightFront = 2;
+	public static final int SPARK_driveRightBack = 2;
 }
