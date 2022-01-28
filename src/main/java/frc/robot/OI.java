b269b5d9b8cb283244101e411a3573e41227adb9
Matthew Epshtein <epshteinmatthew@gmail.com>
2022-01-28 10:47:21 -0800

    chaged OI and robot map to (try to) map my command

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,13 +1,21 @@
 package frc.robot;
-//figure out whats wrong with button imports(https://github.com/frc3268/InfiniteRecharge2020/blob/master/src/main/java/frc/robot/OI.java)
-
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.button.*;
+import frc.robot.commands.TimedAutonomousArcadeDrive;
+import frc.robot.subsystems.DriveTrain;
 
 public class OI {
     //define buttons using robotmap ports stuff
+    //public static Button jb1 = new JoystickButton(joystick, buttonNumber)
+    public static Joystick stick = new Joystick(RobotMap.JOYSTICK_PORT);
+    public static Joystick controller = new Joystick(RobotMap.BIG_CONTROLLER);
+    public static Button button_one = new JoystickButton(stick, RobotMap.FORWARD);
     
 
     public OI(){
         //Map commands to buttons here
+        button_one.whenPressed(new TimedAutonomousArcadeDrive(new DriveTrain(), 5.0, 0.0, 0.5));
+        //I think this may work??? Not exactly sure tho...
     }
     
 }
--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,11 +1,17 @@
 package frc.robot;
 
 public class RobotMap {
-    //OI-Buttons on joystick(?)
+    //JOYSTICK STUFF
+    public static final int JOYSTICK_PORT = 0;
+	public static final int BIG_CONTROLLER = 1;
 
+	public static final int FORWARD = 2;
 
-    //Drive-Motor controllers(Replace with actual ports at some point)
-    public static final int SPARK_driveLeftFront = 1;
+	public static final int SHOOTLOAD = 9;
+
+    //SPARK MOTOR CONTROLLERS
+
+	public static final int SPARK_driveLeftFront = 1;
 	public static final int SPARK_driveLeftBack = 1;
 	public static final int SPARK_driveRightFront = 2;
 	public static final int SPARK_driveRightBack = 2;
