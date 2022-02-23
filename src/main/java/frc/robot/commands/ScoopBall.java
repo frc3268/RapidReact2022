8a2328b7b40584f4877a1c09008fdf8502a4030e
chris_is_a_d--- <ihsrobotics2@gmail.com>
2022-02-23 13:45:36 -0800

    dfgjhwegiohgqEPUIGHQWJHIRGQ3; k  o;iy kuj n tih';kjl./lo apiohfpo23 fo;

--- src/main/java/frc/robot/commands/ScoopBall.java
+++ src/main/java/frc/robot/commands/ScoopBall.java
@@ -5,7 +5,6 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.Scooper;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
