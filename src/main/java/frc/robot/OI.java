84769244a0ed460a4fd713a6994ee67295d2b4c4
chargde_porcupine <robotmeatsuit@gmail.com>
2022-02-23 10:54:34 -0800

    OI fix

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -14,7 +14,7 @@ public class OI {
 
     public OI(){
         //Map commands to buttons here
-        button_one.whenPressed(new TurboBoost(2));
+        button_one.whileHeld(new TurboBoost(2));
         button_one.whenReleased(new JoystickArcadeDrive());
     }
     
