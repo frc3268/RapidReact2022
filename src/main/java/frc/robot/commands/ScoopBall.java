7ba8383bfe0b17436d69c94869a21911d36eee4e
chargde_porcupine <robotmeatsuit@gmail.com>
2022-02-24 10:04:52 -0800

    scooper set

--- src/main/java/frc/robot/commands/ScoopBall.java
+++ src/main/java/frc/robot/commands/ScoopBall.java
@@ -32,6 +32,7 @@ public class ScoopBall extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+      scooperSubsystem.setScoop(1.0);
     
   }
 
