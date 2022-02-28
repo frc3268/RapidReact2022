e079cf36d4eb667252ad8d3ea16cc6d71a494f77
chris_is_a_d--- <ihsrobotics2@gmail.com>
2022-02-28 10:28:37 -0800

    updated motor mapping to be consistent with internal wiring

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -10,8 +10,8 @@ public class RobotMap {
 
     //SPARK MOTOR CONTROLLERS
     // 4, 6, 7 empty
-	public static final int SPARK_driveLeftFront = 1;
-	public static final int SPARK_driveLeftBack = 1;
+	public static final int SPARK_driveLeftFront = 3;
+	public static final int SPARK_driveLeftBack = 3;
 	public static final int SPARK_driveRightFront = 2;
 	public static final int SPARK_driveRightBack = 2;
 	public static final int TALON_scoopBall = 4;
