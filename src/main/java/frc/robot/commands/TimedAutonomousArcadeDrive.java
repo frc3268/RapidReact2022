e478496863ffc5c997f9dbfbb7f68f96cd5087ad
chargde_porcupine <robotmeatsuit@gmail.com>
2022-03-05 13:23:42 -0800

    Edited timed auto to account for inversion

--- src/main/java/frc/robot/commands/TimedAutonomousArcadeDrive.java
+++ src/main/java/frc/robot/commands/TimedAutonomousArcadeDrive.java
@@ -42,7 +42,7 @@ public class TimedAutonomousArcadeDrive extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    driveTrainSubsystem.arcadeDrive(speed, heading);
+    driveTrainSubsystem.arcadeDrive(speed*-0.75, heading*-0.75);
   }
 
   // Called once the command ends or is interrupted.
