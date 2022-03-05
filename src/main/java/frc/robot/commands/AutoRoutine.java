8bb2dd7c781b63a5053b026c5f33669d86dd417a
chargde_porcupine <robotmeatsuit@gmail.com>
2022-03-05 11:12:52 -0800

    lkyuckuy

--- src/main/java/frc/robot/commands/AutoRoutine.java
+++ src/main/java/frc/robot/commands/AutoRoutine.java
@@ -8,11 +8,10 @@ public class AutoRoutine extends SequentialCommandGroup{
     public AutoRoutine(){
         addCommands(
             new TimedAutonomousArcadeDrive(0, 0, 0.5).withTimeout(1),
-            new ShootBall(-0.1).withTimeout(0.5),
+            new ShootBall(-0.2).withTimeout(0.5),
             new TimedAutonomousArcadeDrive(0, 0, 0.5).withInterrupt(() -> RobotContainer.r_powerDistributor.getTotalCurrent() < 6.8),
-            new FullScoop(),
-            new ShootBall(-0.1).withTimeout(0.5),
-            new TimedAutonomousArcadeDrive(0, 1, 0.5)
+            new TimedAutonomousArcadeDrive(0, 1, 0.5),
+            new ShootBall(-0.2).withTimeout(0.5)
         );
     }
     
