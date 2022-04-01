492242479ffcca8170bd1c78e996e2cde027e6cd
ihsrobotics4 <ihsrobotics4@gmail.com>
2022-03-31 17:27:28 -0700

    stuff

--- src/main/java/frc/robot/commands/FullShoot.java
+++ src/main/java/frc/robot/commands/FullShoot.java
@@ -2,12 +2,20 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.RobotContainer;
 
-public class FullShoot extends ParallelCommandGroup{
+public class FullShoot extends SequentialCommandGroup{
     public FullShoot(){
-        addCommands(new MotorSet(-0.5, RobotContainer.r_indexOneSubsystem).withTimeout(2),
-         new MotorSet(-0.5, RobotContainer.r_indexTwoSubsystsem).withTimeout(2));
+        addCommands(
+            new ShooterStartCommand(1),
+            new WaitCommand(1),
+            new MotorSet(0.5, RobotContainer.r_indexOneSubsystem),
+            new MotorSet(-0.5, RobotContainer.r_indexTwoSubsystsem),
+            new WaitCommand(3),
+            new ShooterStartCommand(1),
+            new MotorSet(0.5, RobotContainer.r_indexOneSubsystem),
+            new MotorSet(-0.5, RobotContainer.r_ScooperSubsystem));
     }
     
 }
